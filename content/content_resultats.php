e51047648c6997c4eb3eab44da76d77f7218f426
hugodob <hugo.dobbelaere@polytechnique.edu>
2017-01-13 15:20:02 +0100

    hugo last change

--- content/content_resultats.php
+++ content/content_resultats.php
@@ -9,26 +9,27 @@ echo <<<FIN
                 <p class="lead"><h4>Obtenir le résultat d'un vote</h4></p>
                 </br></br>
             </div>
-            
+
             </br>
 
-            
-            <form class="form-inline">
+
+            <form class="form-inline" action="index.php?page=resultats" id="decode-form">
                 <p>
                     </br>
                     <label>Résultat</label>
-                    <input type="text" name="vote" class="form-control" ng-model="resultatChiffre" />
+                    <input type="text" name="input-resultats" class="form-control" id="input-resultats" />
                     </br>
                     <label>Clé publique</label>
-                    <input type="text" name="token" class="form-control" ng-model="key" />
+                    <input type="text" name="key" class="form-control" id="key" />
                     </br>
                     <label>Clé privée</label>
-                    <input type="text" name="token" class="form-control" ng-model="secKey" />
+                    <input type="text" name="secKey" class="form-control" id="secKey" />
                     </br></br>
-                    <input type="submit" ng-click="decode()" value="Déchiffrer" />
+                    <input type="submit" value="Déchiffrer" />
+                    </br>
                 </p>
             </form>
-            <p> {{ resultat.toString() }} </p>   
+            <div id="zone-results"> </div>
             </br>
 			</br>
 
@@ -38,38 +39,29 @@ echo <<<FIN
                         </br>
 
 
-                            <form class="form-inline" action="index.php?page=resultats" method="post">
+                            <form class="form-inline" action="index.php?page=resultats" method="get" id="somme-form">
 
                                 <div class="form-group">
-                                        
-                                    <label for="exampleInputName2">Entrez l'identifiant de votre question</label>
-                                    <input type="number" ng-model="main.prenom" placeholder="Identifiant" id="id" name="id" class="form-control">
+
+                                    <label>Entrez l'identifiant de votre question</label>
+                                    <input type="number" placeholder="Identifiant" id="id" name="id" class="form-control">
                                     </br>
                                     </br>
                                 </div>
 
                                 <div class="form-group col-md-12">
                                     </br>
-                                        <input type="submit" value="envoyer" />
+                                        <input type="submit"  value="envoyer" />
                                     </br>
                                 </div>
                             </form>
-                            <p> {{ sommeChiffre.toString() }} </p> 
+                            <div id='zone-resultats'> </div>
                         </div>
-                    
+
                 </div>
 FIN;
 
-                    require_once('utilities/bdd.php');
-                    if (isset($_POST['id']))
-                    {
-                        $votes=Votes::getVotes($_POST['id']);
-                    }
-                    else{
-                        $votes=null;
-                    }
-                    
-                    echo "var votes=".json_encode($votes);
+
 echo <<< FIN
 </p>
                 </br>
