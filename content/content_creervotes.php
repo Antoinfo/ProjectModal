246e6e8f2e8c8b2a9c812d24b7340aa88880069a
Antoinfo <new@mail.com>
2017-01-18 13:00:33 +0100

    ajouterquestions

--- content/content_creervotes.php
+++ content/content_creervotes.php
@@ -1,9 +1,13 @@
 <?php
+require('utilities/bdd.php') ;
+require('js/paillier.js') ;
 
 if (!isset($_SESSION['loggedIn'])) {
   echo "Page non autorisée, vous devez vous connecter pour répondre aux questions";
   return;
 }
+
+
 echo <<<FIN
 <div class="container">
 
@@ -45,14 +49,27 @@ echo <<<FIN
                                     </br>
                                     </br>
                                 </div>
+
+
+                                   <br> </br>
                             
-                                <div class="form-group col-md-12">
-                                    <label for="exampleInputEmail2">Clé publique</label>
-                                    <input type="text" id="PublicKey" name="publicKey" placeholder="Public key" class="form-control">
-                                
+                                  <div class="form-group">
+                                        
+                                    <label for="exampleInputName2">Choix0</label>
+                                    <input type="text" placeholder="Choix0" id="Question" name="Choix0" class="form-control">
                                     </br>
                                     </br>
                                 </div>
+
+                                <div class="form-group">
+                                        
+                                    <label for="exampleInputName2">Choix1</label>
+                                    <input type="text" placeholder="Choix1" id="Question" name="Choix1" class="form-control">
+                                    </br>
+                                    </br>
+                                </div>
+
+
 
                                 <div class="form-group col-md-12">
                                     </br>
@@ -63,6 +80,9 @@ echo <<<FIN
                             </form>
                         </div>
                         </br></br>
+
+
+$pub;
 FIN;
 
 
@@ -72,9 +92,9 @@ FIN;
 
 
 
-    if (isset($_POST['Question']) AND isset($_POST['publicKey']))
+    if (isset($_POST['Question']) AND isset($_POST['Choix0']) AND isset($_POST['Choix1']))
     {
-            Questions::addQuestions($dbh, $_POST['Question'], $_POST['publicKey']);
+            Questions::addQuestions($dbh, $_POST['Question'], $_POST['publicKey'], $_POST['Choix0'], $_POST['Choix1']);
     }
     
    
